30c6bb4cdee21e8f6cf15a485791b1fb30696a20
Carlos David <apolayamendozacarlos@gmail.com>
2024-05-19 16:38:01 -0500

    Page Home V.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,27 @@ import React from 'react'
 const page = () => {
   return (
     <div className='container mx-auto'>
-      <div className='flex'>
-        <div className='w-1/2'>
-          <h1>
+      {/* Header */}
+      <div className='flex justify-between'>
+        <div className='w-1/2 flex flex-col gap-y-5 pt-14'>
+          <h1 className='text-6xl'>
             Carlos A. Mendoza
           </h1>
-          <h2>
-            Programador Wen Front-End
+          <h2 className='text-3xl'>
+            Programador Wen Front-End &lt;/&gt;
           </h2>
-          <p>
+          <p className='mr-28 pt-3'>
             Desarrollador web front end con experiencia en diseño y desarrollo de aplicaciones web utilizando
             tecnologías front-end.
           </p>
         </div>
-        <img src="/imagenes/miPerfil.jpg" alt="miPerfil" />
+        <img className='h-72 mr-20' src="/imagenes/miPerfil.jpg" alt="miPerfil" />
       </div>
+
+      {/* Body */}
+      <div></div>
+
+      {/* Footer */}
     </div>
   )
 }
