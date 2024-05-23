944c9230753e676ed05ea9ee80c5885da3eacbe0
Carlos David <apolayamendozacarlos@gmail.com>
2024-05-23 17:26:46 -0500

    MainFooter y MainHeader son Responsive Design

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -1,6 +1,6 @@
 const MainFooter = (): JSX.Element => {
   return (
-    <div className='w-full text-center mb-3'>
+    <div className='w-full text-sm lg:text-base text-center mb-3'>
       <span>
         Creado por Carlos David Apolaya Mendoza
       </span>
